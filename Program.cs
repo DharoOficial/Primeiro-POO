0e8429405d6875aac582d6a8d1a4f781a8aa7007
DharoOficial <erickbandeiradharo@gmail.com>
2020-05-22 10:42:30 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace primeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             personagem capitao = new personagem ();
+            Console.WriteLine("Ataque de capitao: "+capitao.Atacar());
+            Console.WriteLine("Coragem de capitao: " + capitao.coragem);
+            Console.WriteLine("Resestencia de capitao: " + capitao.resistencia);
+        }
+    }
+}
---
+++ personagem.cs
@@ -0,0 +1,17 @@
+namespace primeiroPOO
+{
+    public class personagem
+    {   
+        public int coragem = 98;
+        public int forca = 67;
+        public int resistencia = 78;
+       
+        public string Atacar()
+        {
+            return "o personagem atacou";
+        }
+        
+
+        
+    }
+}
